79df5c18a1ae6bdbfe86adb9cd94aec898012423
Adan Nvir <gasparplanes8@gmail.com>
2026-01-04 16:35:43 -0300

    Fixing imports for the Tiny Transformer

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,9 +1,9 @@
 #Import the class Tensor
-from .tensor import Tensor
+from minitorch.tensor import Tensor
 
 
 ## Import the folder and that they inside have their own imports
-from . import optim
-from . import nn
+from minitorch import optim
+from minitorch import nn
 
 
