309044c5cd40fd19f350dc7e1f8bb7a6babbe571
Adan Nvir <gasparplanes8@gmail.com>
2026-01-04 18:01:12 -0300

    Importation to Transformer as a submodule fixxed

--- minitorch/__init__.py
+++ minitorch/__init__.py
@@ -1,7 +1,7 @@
 #Import the class to use it as a submodule in others projects
-from .minitorch.tensor import Tensor as Tensor
-from .minitorch import optim as optim
-from .minitorch import nn as nn
+from .tensor import Tensor as Tensor
+from . import optim as optim
+from . import nn as nn
 
 
 __all__ = ['Tensor','optim','nn']
